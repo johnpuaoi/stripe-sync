987b35534874b26e95efa430a0a5d255c5c5beea
John Puaoi <jptech@johnpuaoi.tech>
2023-11-01 10:01:35 -1000

    ok

--- packages/stripe-sync/src/utils/eventToResource.ts
+++ packages/stripe-sync/src/utils/eventToResource.ts
@@ -1,29 +1,31 @@
-import invariant from "tiny-invariant";
+import invariant from 'tiny-invariant';
 
 export type Resource = {
   resource: string;
   subresource?: string;
 };
+
 export function eventToResource(event: string): Resource {
-  if (event.includes("checkout.session")) {
+  if (event.includes('checkout.session')) {
     return {
-      resource: "checkout_session",
+      resource: 'stripe_checkout_session',
       subresource: undefined,
     };
   }
-  const split = event.split(".");
+  const split = event.split('.');
   const resource = split.at(-2);
   invariant(resource, `Could not find resource for event: ${event}`);
+
   if (split.length === 2) {
     return {
-      resource,
+      resource: 'stripe_' + resource,
       subresource: undefined,
     };
   }
   if (split.length === 3) {
     return {
-      resource,
-      subresource: split.at(0),
+      resource: 'stripe_' + resource,
+      subresource: 'stripe_' + split.at(0),
     };
   }
   throw new Error(`Could not parse event: ${event}`);
